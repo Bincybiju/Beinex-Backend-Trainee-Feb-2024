68ce54f46cc8114c617caf9edc75096846a4c028
Bincybiju <94056238+Bincybiju@users.noreply.github.com>
2024-02-09 12:53:50 +0530

    Delete week1_task4 directory

--- week1_task4/question4.py
+++
@@ -1,9 +0,0 @@
-user_input = input("Enter a number: ")
-if user_input:
-    number = int(user_input)
-    if number % 2 == 0:
-        print("The number is even.")
-    else:
-        print("The number is odd.")
-else:
-    print("Invalid input. Please enter an integer.")
