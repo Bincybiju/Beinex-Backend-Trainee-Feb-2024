ce64407851d4d4c45bb0f351564d7630408e06fe
Bincybiju <94056238+Bincybiju@users.noreply.github.com>
2024-02-16 17:38:37 +0530

    Update question8.py

--- Week2/week2_task5/question8.py
+++ Week2/week2_task5/question8.py
@@ -1,20 +1,14 @@
 """Create a function that takes a string and returns a dictionary where keys are letters, 
 and values are the number of times each letter appears in the string  """
 
+from collections import Counter
+
 def letters(my_string):
-    count = {}
-    for i in my_string:
-        if i.isalpha():
-            char = i.lower()  
-            if i in count:
-                count[char] += 1
-            else:
-                count[char] = 1
+    str_lower = my_string.lower()    
+    count = Counter(i for i in str_lower if i.isalpha())   
     return count
 
-
 my_string = input("Enter a string: ")
-
 
 result = letters(my_string)
 print("Letter count in the string:", result)
