d6876d14d56bdca1f3317b298628d3d513ed5cde
Bincybiju <94056238+Bincybiju@users.noreply.github.com>
2024-02-20 14:40:18 +0530

    Delete Week3/Task1 directory

--- Week3/Task1/question13.py
+++
@@ -1,8 +0,0 @@
-"""Write a Python program to check if a string contains only digits.("12345" -->True"""
-
-given_string = input("Enter a string: ")
-
-if given_string.isdigit():
-    print(f"The string '{given_string}' contains only digits")
-else:
-    print(f"The string '{given_string}' contains characters other than digits.")
--- Week3/Task1/question21.py
+++
@@ -1,9 +0,0 @@
-"""Write a Python program to remove all whitespace characters from a string.
-"""
-
-
-string = input("Enter a string: ")
-
-string_without_whitespace = ''.join(char for char in string if not char.isspace())
-
-print("String without whitespace:", string_without_whitespace)
